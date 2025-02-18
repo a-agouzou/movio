0dc36d5b124b6670eef36339606c3eb8632a9696
abdellah agouzoul <aagouzou@student.1337.ma>
2025-02-18 22:22:11 +0100

    implement date, raiting formatters

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { Movie } from "@/types/movie";
 import Image from "next/image";
 import React from "react";
+
 interface MovieCardProps {
   movie: Movie;
   onClick: (movie: Movie) => void;
@@ -8,6 +9,17 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie, onClick, observerRef }) => {
+
+  const raitingFormatter : (raiting : Number | null | undefined) => string = (raiting) => {
+    if(raiting === null || raiting === undefined || Number.isNaN(raiting)) return "N/A";
+    return raiting.toFixed(1);
+  }
+
+  const dateFormatter = (dateString: string | null | undefined) => {
+    if (!dateString) return 'N/A';
+    const date = new Date(dateString);
+    return isNaN(date.getFullYear()) ? 'N/A' : date.getFullYear().toString();
+  };
   return (
     <div
       className="cursor-pointer bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300"
@@ -23,18 +35,20 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie, onClick, observerRef }) =>
           }
           alt={movie.title}
           width={300}
+          priority
           height={400}
           className="w-full h-full object-cover"
+
         />
       </div>
       <div className="p-4">
         <h3 className="font-semibold text-lg truncate">{movie.title}</h3>
         <p className="text-sm text-gray-500">
-          {new Date(movie.release_date).getFullYear()}
+          {dateFormatter(movie.release_date)}
         </p>
         <div className="mt-2 flex items-center">
           <span className="text-yellow-500">★</span>
-          <span className="ml-1">{movie.vote_average.toFixed(1)}</span>
+          <span className="ml-1">{raitingFormatter(movie.vote_average)}</span>
         </div>
       </div>
     </div>
