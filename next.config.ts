9e1d4ab8fd66d5f3084fe023dbe0795a2e60e483
abdellah agouzoul <aagouzou@student.1337.ma>
2025-02-18 23:48:47 +0100

    update next.config from domains to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains:  ["image.tmdb.org", "placehold.co"],
+    remotePatterns : [{
+      protocol: "https",
+      hostname: "image.tmdb.org",
+    }],
   },
 };
 
